ef7b3b4dae8987ea4391af31b3fbff45e04b41a4
Maxim Osipov <joper1234@yandex.com>
2020-07-02 22:58:34 +0500

    easy

--- app.js
+++ app.js
@@ -3,33 +3,39 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filter = document.querySelector(".filter-todo");
 
+document.addEventListener("DOMContentLoaded", GetToDos());
 todoButton.addEventListener("click", addToDo);
 todoList.addEventListener("click", deleteCheck);
 filter.addEventListener("click", filterToDo);
 
 function addToDo(e) {
   e.preventDefault();
+  if (todoInput.value.trim() !== "") {
+    const toDoDiv = document.createElement("div");
+    toDoDiv.classList.add("todo");
 
-  const toDoDiv = document.createElement("div");
-  toDoDiv.classList.add("todo");
+    const newToDo = document.createElement("li");
+    newToDo.innerText = todoInput.value;
+    newToDo.classList.add("todo-item");
+    toDoDiv.appendChild(newToDo);
 
-  const newToDo = document.createElement("li");
-  newToDo.innerText = todoInput.value;
-  newToDo.classList.add("todo-item");
-  toDoDiv.appendChild(newToDo);
+    SaveLocalToDos(todoInput.value);
 
-  const completedButton = document.createElement("button");
-  completedButton.innerHTML = '<i class = "fas fa-check"></i>';
-  completedButton.classList.add("complete-btn");
-  toDoDiv.appendChild(completedButton);
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    toDoDiv.appendChild(completedButton);
 
-  const dltdButton = document.createElement("button");
-  dltdButton.innerHTML = '<i class = "fas fa-trash"></i>';
-  dltdButton.classList.add("delete-btn");
-  toDoDiv.appendChild(dltdButton);
+    const dltdButton = document.createElement("button");
+    dltdButton.innerHTML = '<i class = "fas fa-trash"></i>';
+    dltdButton.classList.add("delete-btn");
+    toDoDiv.appendChild(dltdButton);
 
-  todoList.appendChild(toDoDiv);
-  todoInput.value = "";
+    todoList.appendChild(toDoDiv);
+    todoInput.value = "";
+  } else {
+    alert("Empty");
+  }
 }
 
 function deleteCheck(e) {
@@ -37,6 +43,7 @@ function deleteCheck(e) {
   if (item.classList[0] === "delete-btn") {
     const todo = item.parentElement;
     todo.classList.add("fall");
+    RemoveLocalToDos(todo);
     todo.addEventListener("transitionend", function () {
       todo.remove();
     });
@@ -70,4 +77,58 @@ function filterToDo(e) {
         break;
     }
   });
+}
+
+function SaveLocalToDos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function GetToDos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function (todo) {
+    const toDoDiv = document.createElement("div");
+    toDoDiv.classList.add("todo");
+
+    const newToDo = document.createElement("li");
+    newToDo.innerText = todo;
+    newToDo.classList.add("todo-item");
+    toDoDiv.appendChild(newToDo);
+
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    toDoDiv.appendChild(completedButton);
+
+    const dltdButton = document.createElement("button");
+    dltdButton.innerHTML = '<i class = "fas fa-trash"></i>';
+    dltdButton.classList.add("delete-btn");
+    toDoDiv.appendChild(dltdButton);
+
+    todoList.appendChild(toDoDiv);
+    todoInput.value = "";
+  });
+}
+
+function RemoveLocalToDos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
